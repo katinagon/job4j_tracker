46062a42dde2d8df989d7cd4c29be50257f729a5
Ekaterina Goncharova <katinagon@yandex.ru>
2023-02-13 14:35:01 +0800

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,32 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] tempArray = new Item[items.length];
+        int size = 0;
+        for (Item item : items) {
+            if (item != null) {
+                tempArray[size] = item;
+                size++;
+            }
+        }
+        tempArray = Arrays.copyOf(tempArray, size);
+        return tempArray;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] tempArray = new Item[items.length];
+        int size = 0;
+        for (Item item : items) {
+            if (item != null && key.equals(item.getName())) {
+                tempArray[size] = item;
+                size++;
+            }
+        }
+        tempArray = Arrays.copyOf(tempArray, size);
+        return tempArray;
     }
 
     public Item findById(int id) {

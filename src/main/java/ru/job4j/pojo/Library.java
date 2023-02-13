87b984acd467f869ac66d2af3655be72187bdd87
Ekaterina Goncharova <katinagon@yandex.ru>
2023-02-13 14:05:17 +0800

    3. Массивы и модели. [#174892 #390992]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -6,15 +6,18 @@ public class Library {
         Book bookTwo = new Book("Clean code", 464);
         Book bookThree = new Book("Harry Potter and the Sorcerer's Stone", 432);
         Book bookFour = new Book("Crime and Punishment", 592);
+
         Book[] books = new Book[4];
         books[0] = bookOne;
         books[1] = bookTwo;
         books[2] = bookThree;
         books[3] = bookFour;
+
         for (Book book : books) {
             System.out.println(book.getName() + " - " + book.getNumberOfPages());
         }
         System.out.println();
+
         books[0] = books[3];
         books[3] = bookOne;
         System.out.println("После перестановки книг:");
@@ -22,6 +25,7 @@ public class Library {
             System.out.println(book.getName() + " - " + book.getNumberOfPages());
         }
         System.out.println();
+
         System.out.println("Вывод книг с именем \"Clean code\":");
         for (Book book : books) {
             if ("Clean code".equals(book.getName())) {

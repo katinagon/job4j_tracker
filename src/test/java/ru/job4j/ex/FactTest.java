d8881c0869cfeb8cffd0c79f457c7911ca8b0c30
Ekaterina Goncharova <katinagon@yandex.ru>
2023-02-20 12:58:21 +0800

    0.3. Тестирование исключений с junit [#219366 #393331]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -25,4 +25,16 @@ class FactTest {
                 });
         assertThat(exception.getMessage()).isEqualTo("N could not be less then 0");
     }
+
+    @Test
+    public void whenCorrectResult6() {
+        int n = 3;
+        assertThat(new Fact().calc(n)).isEqualTo(6);
+    }
+
+    @Test
+    public void whenCorrectResult2() {
+        int n = 2;
+        assertThat(new Fact().calc(n)).isEqualTo(2);
+    }
 }

6d3acffe2ba10498e2da8a54a8149d5fc38e8f93
Ekaterina Goncharova <katinagon@yandex.ru>
2023-02-21 08:49:09 +0800

    4.2. Интерфейс Input [#181092 #392172]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,16 +4,17 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
-
+        System.out.println("Автобус едет по дороге.");
     }
 
     @Override
     public void passengers(int number) {
-
+        System.out.println("В автобусе " + number + " пассажиров.");
     }
 
     @Override
     public double refuel(double fuel) {
-        return 0;
+        double price = 55.48;
+        return price * fuel;
     }
 }
